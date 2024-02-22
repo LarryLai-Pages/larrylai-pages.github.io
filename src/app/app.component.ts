826759ea08c083837342b07e19169e02e200c557
Larry.Lai <Larry.Lai@portwell.com.tw>
2024-02-22 13:57:02 +0800

    edit src/app/app 加入版本資訊

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { config } from 'src/config';
+import { PackageService } from './services/package';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,11 @@ import { config } from 'src/config';
 })
 export class AppComponent {
   constructor(
-    private Title:Title
+    private Title:Title,
+    private package_json:PackageService,
   ){
     this.Title.setTitle(this.title)
   }
   get title():string{return config.title}
-
+  get version():string{return this.package_json.version}
 }
